edc91e65fdb7439125795dcd9b54c03b826d1c33
AnirudhSinghTomar <65367836+AnirudhSinghTomar@users.noreply.github.com>
2021-06-30 05:07:03 +0530

    LEVEL 3

--- Task 3/script.js
+++ Task 3/script.js
@@ -1,24 +1,11 @@
-
-let arr =["p","button","li"];
-let text =["Hey this is Anirudh Singh Tomar","Click to Know me","By"];
-for(let i=0;i<arr.length;i++)
-{
+let arr = ["p", "button", "li"];
+let text = ["Hey this is Anirudh Singh Tomar", "Click to Know me", "By"];
+for (let i = 0; i < arr.length; i++) {
     var node = document.createElement(arr[i]);
-    var textnode = document.createTextNode(text[i]);         // Create a text node
-    node.appendChild(textnode); 
+    var textnode = document.createTextNode(text[i]); // Create a text node
+    node.appendChild(textnode);
     var element = document.getElementById("body");
-    element.appendChild(node); 
-    console.log("Added a new node",arr[i]);                           // Append the text to <li>
-    
+    element.appendChild(node);
+    console.log("Added a new node", arr[i]); // Append the text to <li>
+
 }
-
-// var list =document.getElementById("body");
-// for(let  j=0;list[j];j++)
-// {
-//     console.log(list[j]);
-// }
-// var all = document.getElementsByTagName("body");
-
-// for (var i=0, max=all.length; i < max; i++) {
-//     console.log("Added element",all[i]);
-// }
